81ac9cb63d57dc77dca615ae8771bac88722ad79
Adrija013 <adrijabondyopadhyay9604@gmail.com>
2025-07-05 21:34:17 +0530

    initial commit

---
+++ project 1/script.js
@@ -0,0 +1,7 @@
+function myfunction() {
+    var a = 4
+    document.getElementById("result").innerHTML=a * a
+    
+}
+var text = document.getElementById("demo").innerHTML;
+        alert("The first heading is " + text);
