7ae029943fa064ab3da3bd1a426fd1ca938df295
Adrija013 <adrijabondyopadhyay9604@gmail.com>
2025-09-16 23:40:42 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,15 @@
+first_name = input("Enter your first name : ")
+last_name = input("Enter your last name : ")
+age = input("Enter your age : ")
+height = input("Enter your height : ")
+weight = input("Enter your weight : ")
+fav_subject = input("Enter your favourite subject : ")
+
+p = (first_name , last_name , age , height , weight , fav_subject)
+
+l = list(p)
+
+print("\n tupple : ")
+print(p)
+print("\n List : ")
+print(l)
